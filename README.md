fb35fbe9868f854daedddd914d5055c2f1879701
Vishal Hasnani <hasnanivishal@gmail.com>
2024-05-11 17:22:11 +0530

    Updated ReadMe.md file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Onion-Architecture
+
+* Applicaton Flow
+
+- Core -> Domain And Service
+- Infrastructure -> Persistence And Presentation
+- Web Project (Host)
--- Service/Extensions.cs
+++ Service/Extensions.cs
@@ -1,24 +1,15 @@
 ﻿using Contract;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Service
+namespace Service;
+
+public static class Extensions
 {
-    public static class Extensions
+    public static PersonDto ToPersonDto(this Person person)
     {
-        public static PersonDto ToPersonDto(this Person person)
-        {
-            return new PersonDto()
-            {
-                Id = person.Id,
-                Name = person.Name,
-                Address = person.Address,
-                DateOfBirth = person.DateOfBirth,
-            };
-        }
+        return new PersonDto(person.Id,
+            person.Name,
+            person.DateOfBirth,
+            person.Address);
     }
 }
